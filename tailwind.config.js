11cf6e63d4171cd72156501a4d43fafa91c88773
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-05-29 16:56:51 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["**/*.{html,js}"], // :point_left:
+  theme: {
+    extend: {
+      colors: {
+        'darkGray': '#1F293B',
+        'blueDark': '#0F172A',
+      },
+    },
+  },
+  plugins: [],
+};
